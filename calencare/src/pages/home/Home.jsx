a1c6462276017da13b359c2e5c33baa662436b24
raquel-ogs <raquel.silva@sptech.school>
2024-04-25 20:08:44 -0300

    feat: adição de page home

--- calencare/src/pages/home/Home.jsx
+++ calencare/src/pages/home/Home.jsx
@@ -1,10 +1,24 @@
 import React from "react";
-import "./Home.module.css";
+import styles from "./Home.module.css";
+import Titulo from "../../components/titulo/Titulo";
+import Navbar from "../../components/navbar/Navbar";
+import imgInicio from "../../utils/assets/inicio.svg";
+
 
 const Home = () => {
   return (
     <>
-      <h1> Page home </h1>
+      <div className={styles["page-home"]}>
+        <Navbar />
+        <section className={styles["section-inicio"]}>
+          <div className={styles["text"]}>
+            <h1 className={styles["title"]}> Organizando seu negócio, sempre que <span className={styles["title-roxo"]}> você </span> quiser </h1>
+          </div>
+          <div className={styles["image"]}>
+            <img src={imgInicio} alt="Ilustração mulher realizando agendamento" className={styles["img-inicio"]} />
+          </div>
+        </section>
+      </div>
     </>
   );
 }
