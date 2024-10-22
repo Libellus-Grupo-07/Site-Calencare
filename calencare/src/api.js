fa32bd6401c762e888041269ee177f230b6c3d01
Torres5271 <gustavo.souza@sptech.school>
2024-10-21 21:00:50 -0300

    subindo front

--- calencare/src/api.js
+++ calencare/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const api = axios.create({
-    baseURL: "https://3.216.72.5:5225/"
+    baseURL: "https://calencare-prod.zapto.org/api"
 });
 
 export default api;
