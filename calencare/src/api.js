4fcc18538b36a7b8be879624527ac07b4587567f
torres5271 <gustavo.souza@sptech.school>
2024-10-26 20:32:10 -0300

    api.js

--- calencare/src/api.js
+++ calencare/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 const api = axios.create({
-    baseURL: "http://10.0.0.199"
+    baseURL: "http://10.0.0.199:8080/"
 });
 
 export default api;
