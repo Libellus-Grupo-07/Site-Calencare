cec739dcc72be92cb3aebc62068b40b78230c207
Luma Souza <125762406+lumasouza@users.noreply.github.com>
2024-10-28 00:26:50 -0300

    modificando baseURL

--- calencare/src/api.js
+++ calencare/src/api.js
@@ -2,12 +2,8 @@ import axios from "axios";
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-// const api = axios.create({
-//     baseURL: "http://54.243.135.89:8080/api/"
-// });
-
 const api = axios.create({
-    baseURL: "http://localhost:8080/api/"
+     baseURL: "http://54.243.135.89:8080/api/"
 });
 
 export default api;
