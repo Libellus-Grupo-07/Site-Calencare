5d7c8ff60376c868e881b072b8bfd8cf4960746f
Luma Souza <luma.souza@sptech.school>
2024-10-28 01:17:16 -0300

    teste 3

--- calencare/src/api.js
+++ calencare/src/api.js
@@ -2,9 +2,14 @@ import axios from "axios";
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-
 const api = axios.create({
-     baseURL: "http://54.243.135.89:8080/api/"
+    baseURL: "http://54.243.135.89:8080/api/"
 });
 
+
+
+
+
+
 export default api;
+
